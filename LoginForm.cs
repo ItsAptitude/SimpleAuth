7912497d22701f5601211f2674f45b65c1245396
Aptitude <th3o@live.se>
2021-07-05 14:33:08 +0200

    Add project files.

---
+++ LoginForm.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Net;
+using System.Security.Principal;
+using System.Windows.Forms;
+
+namespace FreeandPremium
+{
+    public partial class LoginForm : Form
+    {
+        string hwid = WindowsIdentity.GetCurrent().User.Value;
+        WebClient loginshit = new WebClient();
+
+
+        public LoginForm()
+        {
+            InitializeComponent();
+            HttpWebRequest.DefaultWebProxy = new WebProxy(); // disable default proxy
+
+        }
+
+        private void FreeLoginBtn_Click(object sender, EventArgs e)
+        {
+            WebClient loginshit = new WebClient();
+            string hwidshit = loginshit.DownloadString("https://pastebin.com/raw/wX50fHzk"); // Edit and change to your pastebin
+            if (hwidshit.Contains(hwid))
+            {
+                MessageBox.Show("You have premium version! Go use it!", "https://github.com/ItsAptitude");
+            }
+            else
+            {
+                MainForm main = new MainForm();
+                this.Hide();
+                main.Show();
+            }
+        }
+       
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            hwidtextbox.Text = hwid;
+            hwid = hwid;
+        }
+
+        private void PremiumLoginBtn_Click(object sender, EventArgs e)
+        {
+            string hwidshit = loginshit.DownloadString("https://pastebin.com/raw/5XVM2Rmm"); // Edit and change to your pastebin
+            if (hwidshit.Contains(hwid))
+            {
+                MainForm main = new MainForm();
+                main.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("You don't have premium version!", "https://github.com/ItsAptitude");
+            }
+        }
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Security.Principal;
+using System.Windows.Forms;
+
+namespace FreeandPremium
+{
+    public partial class MainForm : Form
+    {
+        string hwid = WindowsIdentity.GetCurrent().User.Value;
+        WebClient loginshit = new WebClient();
+        bool premium = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            HttpWebRequest.DefaultWebProxy = new WebProxy(); // disable default proxy to hide from http debuggers
+            string hwidshit = loginshit.DownloadString("https://pastebin.com/raw/5XVM2Rmm");
+            if (hwidshit.Contains(hwid))
+            {
+                premium = true;
+            }
+        }
+
+        private void ggb_Click(object sender, EventArgs e)
+        {
+            // Free and Premium
+            Process.Start("https://www.google.com/");   
+        }
+
+        private void fbb_Click(object sender, EventArgs e)
+        {
+            // Free and Premium
+            Process.Start("https://www.facebook.com/");            
+        }
+
+        private void wab_Click(object sender, EventArgs e)
+        {
+            // Free and Premium
+            Process.Start("https://www.whatsapp.com/");            
+        }
+
+        private void twb_Click(object sender, EventArgs e)
+        {
+            // Premium
+            if (premium == true)
+            {
+                Process.Start("https://twitter.com/");
+            }
+            else
+            {
+                MessageBox.Show("You're not Premium !");
+            }
+        }
+
+        private void ytb_Click(object sender, EventArgs e)
+        {
+            // Premium
+            if (premium == true)
+            {
+                Process.Start("https://www.youtube.com/");
+            }
+            else
+            {
+                MessageBox.Show("You're not Premium !");
+            }
+        }
+
+        private void igb_Click(object sender, EventArgs e)
+        {
+            // Premium
+            if (premium == true)
+            {
+                Process.Start("https://www.instagram.com/");
+            }
+            else
+            {
+                MessageBox.Show("You're not Premium !");
+            }
+        }
+    }
+}
